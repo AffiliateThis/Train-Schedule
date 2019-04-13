71cd6121f9d711d9e49890fc6dd7f752bf7ab978
Affiliatethis <chris@onecrookedtree.com>
2019-04-13 10:05:00 -0400

    Final Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,9 +16,113 @@ var database = firebase.database();
 var connectionsRef = database.ref("/connections");
 
 // The button to add Trains
-$("#add-train-btn").on("click", function (event) {
+$("#add-train-btn").off("click").on("click", function (event) {
     event.preventDefault();
 
-    console.log(event)
+    // console.log(event)
 
-})
+    var trainFirstTime = moment($("#first-time-input").val(), 'HH:mm');
+    // Grabbing the user's input, using .val().trim()
+
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var trainFirstTimeFormatted = trainFirstTime.format("HH:mm");
+    var trainFrequency = $("#frequency-input").val().trim();
+    var currentTime = moment();
+
+    if (trainFirstTime > currentTime) {
+        nextTrain = trainFirstTime;
+    } else {
+        var minutesPast = currentTime.diff(trainFirstTime, 'minutes');
+        var remainder = minutesPast % trainFrequency;
+        var minutesTilNextTrain = trainFrequency - remainder;
+        nextTrain = currentTime.add(minutesTilNextTrain, 'minutes');
+    }
+
+    //   Temporary data via creating an object.
+
+    var newTrain = {
+        name: trainName,
+        dest: trainDest,
+        depart: trainFirstTimeFormatted,
+        arrival: trainFrequency,
+        nextTrain: nextTrain.format('HH:mm'),
+
+    };
+
+    // console.log(newTrain)
+
+    // uploads the train data to the database
+    database.ref().push(newTrain);
+
+    // console log of current data points collected/entered
+    // console.log(newTrain.name);
+    // console.log(newTrain.dest);
+    // console.log(newTrain.depart);
+    // console.log(newTrain.arrival);
+
+    // confirmation of the train being added once console logs are run.
+
+    alert("Train successfully added");
+
+    // This clears all text fields info
+
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-time-input").val("");
+    $("#frequency-input").val("");
+    // $("#arrival-input").val("");
+});
+
+database.ref().on("child_added", function (childSnapshot) {
+    // console.log(childSnapshot.val());
+
+
+
+    // Store everything into a variable
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().dest;
+    var trainDepart = childSnapshot.val().depart;
+    var trainFrequency = childSnapshot.val().arrival;
+    var nextTrainEta = childSnapshot.val().nextTrain;
+
+    // console.log(trainName);
+    // console.log(trainDest);
+    // console.log(trainDepart);
+    // console.log(trainFrequency);
+    // console.log(nextTrainEta);
+
+
+
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDest),
+        $("<td>").text(trainDepart),
+        $("<td>").text(trainFrequency),
+        $("<td>").text(nextTrainEta),
+        // $("<td>").text(trainArrive),
+        // $("<td>").text(duration),
+        // $("<td>").text(eta),
+    );
+
+
+    $("#train-table > tbody").append(newRow);
+
+    // format.moment();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
